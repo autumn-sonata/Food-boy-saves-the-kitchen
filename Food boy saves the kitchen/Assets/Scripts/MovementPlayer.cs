4abd9448ca394d47d4689c91c79b76be03cca8d5
Chick3nBoy <leonardo.ongdc@gmail.com>
2022-05-23 14:12:18 +0800

    Added sprites and player tag

--- Food boy saves the kitchen/Assets/Scripts/MovementPlayer.cs
+++ Food boy saves the kitchen/Assets/Scripts/MovementPlayer.cs
@@ -6,48 +6,62 @@ public class MovementPlayer : MonoBehaviour
 {
     public float playerSpeed = 7f;
     public Transform destination;
-    public LayerMask collisionType;
+    public LayerMask collisionHeavy;
+    public LayerMask collisionPush;
     private float spriteSize;
-    private float timeDelayPerMovement = 0.1f;
-    private float timeToMoveAgain = 0f;
+
+    //Pushes object in 
+    public void Collide(float horizontalMove, float verticalMove)
+    {
+        if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, verticalMove, 0f), .3f, collisionHeavy))
+        {
+            destination.position = transform.position + new Vector3(horizontalMove * spriteSize, verticalMove * spriteSize, 0f);
+        }
+        else if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, verticalMove, 0f), .3f, collisionPush))
+        {
+            Collide(horizontalMove, verticalMove);
+        }
+
+    }
     // Start is called before the first frame update
     void Start()
     {
         spriteSize = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
         destination.SetParent(null);
-        //Debug.Log(destination.position);
-        //Debug.Log(transform.position);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal");
-        float verticalMove = Input.GetAxisRaw("Vertical");
-        // Debug.Log(horizontalMove);
-        // Debug.Log(verticalMove);
-        // Debug.Log(destination.position);
-        // Debug.Log(transform.position);
+        if (gameObject.tag == "Player")
+        {
+            float horizontalMove = Input.GetAxisRaw("Horizontal");
+            float verticalMove = Input.GetAxisRaw("Vertical");
 
-        transform.position = Vector3.MoveTowards(transform.position, destination.position, spriteSize * Time.deltaTime * playerSpeed);
-        //TODO ADD A DELAY AFTER REACHING DEST
-        if (Vector3.Distance(destination.position, transform.position) <= 0.05f)
-        {
-            //update destination position
-            if (Mathf.Abs(horizontalMove) == 1f && 
-            !Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove * spriteSize, 0f, 0f), .3f, collisionType) &&
-            Time.time >= timeToMoveAgain) 
+            //moves object to destination
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, playerSpeed * Time.deltaTime);
+
+            //checks for input and moves destination accordingly
+            if (Vector3.Distance(destination.position, transform.position) == 0f)
             {
-                destination.position += new Vector3(horizontalMove * spriteSize, 0f, 0f);
-            } else if (Mathf.Abs(verticalMove) == 1f && 
-            !Physics2D.OverlapCircle(destination.position + new Vector3(0f, verticalMove * spriteSize, 0f), .3f, collisionType) &&
-            Time.time >= timeToMoveAgain)
-            {
-                destination.position += new Vector3(0f, verticalMove * spriteSize, 0f);
+                if (Mathf.Abs(horizontalMove) == 1f)
+                {
+                    //checks for LayerMask of collisionType "Heavy" in destination, if present don't move
+                    //if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, 0f, 0f), .3f, collisionHeavy))
+                    //{
+                    //    destination.position = transform.position + new Vector3(horizontalMove * spriteSize, 0f, 0f);
+                    //}
+                    Collide(horizontalMove, 0f);
+                }
+                else if (Mathf.Abs(verticalMove) == 1f)
+                {
+                    //if (!Physics2D.OverlapCircle(destination.position + new Vector3(0f, verticalMove * spriteSize, 0f), .3f, collisionHeavy))
+                    //{
+                    //    destination.position = transform.position + new Vector3(0f, verticalMove * spriteSize, 0f);
+                    //}
+                    Collide(0f, verticalMove);
+                }
             }
-        } else 
-        {
-            timeToMoveAgain = Time.time + timeDelayPerMovement;
-            //Debug.Log(timeToMoveAgain);
         }
     }
 }
