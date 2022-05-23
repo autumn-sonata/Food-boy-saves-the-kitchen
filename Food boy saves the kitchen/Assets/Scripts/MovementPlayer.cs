19fb724f0a9a8686052f1f80e02dfeccfbf0dcdd
Chick3nBoy <leonardo.ongdc@gmail.com>
2022-05-23 22:01:04 +0800

    Added multiple tags script and level selection

--- Food boy saves the kitchen/Assets/Scripts/MovementPlayer.cs
+++ Food boy saves the kitchen/Assets/Scripts/MovementPlayer.cs
@@ -9,59 +9,58 @@ public class MovementPlayer : MonoBehaviour
     public LayerMask collisionHeavy;
     public LayerMask collisionPush;
     private float spriteSize;
-
-    //Pushes object in 
-    public void Collide(float horizontalMove, float verticalMove)
-    {
-        if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, verticalMove, 0f), .3f, collisionHeavy))
-        {
-            destination.position = transform.position + new Vector3(horizontalMove * spriteSize, verticalMove * spriteSize, 0f);
-        }
-        else if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, verticalMove, 0f), .3f, collisionPush))
-        {
-            Collide(horizontalMove, verticalMove);
-        }
-
-    }
+    private float timeDelayPerMovement = 0.1f;
+    private float timeToMoveAgain = 0f;
     // Start is called before the first frame update
     void Start()
     {
         spriteSize = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
         destination.SetParent(null);
+        //Debug.Log(destination.position);
+        //Debug.Log(transform.position);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (gameObject.tag == "Player")
+        if (GetComponent<Tags>().objectTags[1] == "Player")
         {
             float horizontalMove = Input.GetAxisRaw("Horizontal");
             float verticalMove = Input.GetAxisRaw("Vertical");
+            // Debug.Log(horizontalMove);
+            // Debug.Log(verticalMove);
+            // Debug.Log(destination.position);
+            // Debug.Log(transform.position);
 
-            //moves object to destination
-            transform.position = Vector3.MoveTowards(transform.position, destination.position, playerSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, spriteSize * Time.deltaTime * playerSpeed);
 
-            //checks for input and moves destination accordingly
-            if (Vector3.Distance(destination.position, transform.position) == 0f)
+            if (Vector3.Distance(destination.position, transform.position) <= 0.05f)
             {
-                if (Mathf.Abs(horizontalMove) == 1f)
+                //update destination position
+                if (Mathf.Abs(horizontalMove) == 1f && Time.time >= timeToMoveAgain) 
                 {
-                    //checks for LayerMask of collisionType "Heavy" in destination, if present don't move
-                    //if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, 0f, 0f), .3f, collisionHeavy))
-                    //{
-                    //    destination.position = transform.position + new Vector3(horizontalMove * spriteSize, 0f, 0f);
-                    //}
-                    Collide(horizontalMove, 0f);
-                }
-                else if (Mathf.Abs(verticalMove) == 1f)
+                    CollideWithHeavy(horizontalMove, 0f);
+                } else if (Mathf.Abs(verticalMove) == 1f && Time.time >= timeToMoveAgain)
                 {
-                    //if (!Physics2D.OverlapCircle(destination.position + new Vector3(0f, verticalMove * spriteSize, 0f), .3f, collisionHeavy))
-                    //{
-                    //    destination.position = transform.position + new Vector3(0f, verticalMove * spriteSize, 0f);
-                    //}
-                    Collide(0f, verticalMove);
+                    CollideWithHeavy(0f, verticalMove);
                 }
+            } else 
+            {
+                timeToMoveAgain = Time.time + timeDelayPerMovement;
+                //Debug.Log(timeToMoveAgain);
             }
         }
+    }
+
+    public void CollideWithHeavy(float horizontalMove, float verticalMove)
+    {
+        if (!Physics2D.OverlapCircle(destination.position + new Vector3(horizontalMove, verticalMove, 0f), .3f, collisionHeavy))
+        {
+            destination.position = transform.position + new Vector3(horizontalMove * spriteSize, verticalMove * spriteSize, 0f);
+        }
+    }
+
+    public void CollideWithPush()
+    {
+        //Make all pushable objects that are in contact with one another as one object
     }
 }
