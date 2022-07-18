6d72b4cd83ddbf17f65918b219b022d6c3663ff1
Eric Lee Ying Yao <e0694459@u.nus.edu>
2022-07-18 21:29:53 +0800

    Added glow effect to cooked objects

--- Food boy saves the kitchen/Assets/Scripts/Coordinator.cs
+++ Food boy saves the kitchen/Assets/Scripts/Coordinator.cs
@@ -25,6 +25,7 @@ public class Coordinator : MonoBehaviour
     private Timer playerTimer;
     private PastMovesManager moveManager;
     private InputManager inputManager;
+    private GlowManager glowManager;
     private List<SpriteManager> sprites;
     private List<WinManager> winTiles; //All instances of winTiles.
     private List<YouManager> youTiles; //All instances of youTiles.
@@ -45,6 +46,7 @@ public class Coordinator : MonoBehaviour
         playerTimer = GetComponent<Timer>();
         moveManager = GameObject.Find("Canvas").GetComponent<PastMovesManager>();
         inputManager = GetComponent<InputManager>();
+        glowManager = GetComponent<GlowManager>();
         sprites = GetAllSprites();
         winTiles = FindObjectsOfType<WinManager>().ToList();
         youTiles = FindObjectsOfType<YouManager>().ToList();
